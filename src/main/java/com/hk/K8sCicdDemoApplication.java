ca4a8f599d5702a409dcc14ea6e208d0b67810b6
kunhuang12345 <huangkunkhhuangkun@gmail.com>
2024-02-24 23:18:59 +0800

    补充

--- src/main/java/com/hk/K8sCicdDemoApplication.java
+++ src/main/java/com/hk/K8sCicdDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class K8sCicdDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(K8sCicdDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(K8sCicdDemoApplication.class, args);
+    }
 
 }
--- src/main/java/com/hk/controller/UserController.java
+++ src/main/java/com/hk/controller/UserController.java
@@ -1,2 +1,19 @@
-package com.hk.controller;public class UserController {
+package com.hk.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String users(Integer age){
+        log.info("test:{}", age);
+        return "<h1>all users: v1.0.2</h>";
+    }
 }
